8f5c5f6a6207b0476642cc4bfd380adad8054fed
Naveen Kumar Kuppan <naveenkumarn2@gmail.com>
2023-11-27 00:48:28 +0100

    Updating app version

--- build-logic/convention/src/main/java/com/naveenapps/expensemanager/buildsrc/extensions/AndroidConfigExt.kt
+++ build-logic/convention/src/main/java/com/naveenapps/expensemanager/buildsrc/extensions/AndroidConfigExt.kt
@@ -11,8 +11,8 @@ const val TARGET_SDK = 34
 const val COMPILE_SDK: Int = 34
 const val MIN_SDK = 21
 
-const val VERSION_CODE = 5
-const val VERSION_NAME = "1.0.5"
+const val VERSION_CODE = 6
+const val VERSION_NAME = "1.0.6"
 
 const val BENCHMARK_RUNNER = "androidx.test.runner.AndroidJUnitRunner"
 
