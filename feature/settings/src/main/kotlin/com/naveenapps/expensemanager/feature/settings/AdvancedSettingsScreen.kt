28f19dd6e0f124901ed2b4f302ce40797eed7387
Naveen Kumar Kuppan <naveenkumarn2@gmail.com>
2023-12-05 00:40:03 +0100

    Updating github actions and advanced settings category selection view

--- feature/settings/src/main/kotlin/com/naveenapps/expensemanager/feature/settings/AdvancedSettingsScreen.kt
+++ feature/settings/src/main/kotlin/com/naveenapps/expensemanager/feature/settings/AdvancedSettingsScreen.kt
@@ -27,6 +27,8 @@ import com.naveenapps.expensemanager.core.designsystem.ui.components.TopNavigati
 import com.naveenapps.expensemanager.core.designsystem.ui.theme.ExpenseManagerTheme
 import com.naveenapps.expensemanager.core.model.Account
 import com.naveenapps.expensemanager.core.model.AccountType
+import com.naveenapps.expensemanager.core.model.Category
+import com.naveenapps.expensemanager.core.model.CategoryType
 import com.naveenapps.expensemanager.core.model.StoredIcon
 import java.util.Date
 
@@ -36,14 +38,15 @@ fun AdvancedSettingsScreen(
 ) {
     AdvancedSettingsScaffoldView(
         accounts = viewModel.accounts,
+        categories = viewModel.categories,
         backPress = viewModel::closePage,
     )
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 private fun AdvancedSettingsScaffoldView(
     accounts: List<Account>,
+    categories: List<Category>,
     backPress: () -> Unit
 ) {
     Scaffold(
@@ -61,7 +64,19 @@ private fun AdvancedSettingsScaffoldView(
                 .verticalScroll(rememberScrollState())
         ) {
             if (accounts.isNotEmpty()) {
-                DropDownView(accounts)
+                AccountPreSelectionView(
+                    accounts, modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(16.dp)
+                )
+            }
+            if (categories.isNotEmpty()) {
+                CategoryPreSelectionView(
+                    categories,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(16.dp)
+                )
             }
         }
     }
@@ -69,26 +84,29 @@ private fun AdvancedSettingsScaffoldView(
 
 @Composable
 @OptIn(ExperimentalMaterial3Api::class)
-private fun DropDownView(accounts: List<Account>) {
+private fun AccountPreSelectionView(
+    accounts: List<Account>,
+    modifier: Modifier = Modifier
+) {
     var expanded by remember { mutableStateOf(false) }
     var selectedAccount by remember { mutableStateOf(accounts[0]) }
 
     ExposedDropdownMenuBox(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(16.dp),
+        modifier = modifier,
         expanded = expanded,
         onExpandedChange = {
             expanded = expanded.not()
         }
     ) {
         OutlinedTextField(
-            modifier = Modifier.fillMaxWidth(),
+            modifier = Modifier
+                .fillMaxWidth()
+                .menuAnchor(),
             readOnly = true,
             value = selectedAccount.name,
             onValueChange = { },
             label = {
-                Text(stringResource(id = R.string.selected_account))
+                Text(stringResource(id = R.string.default_selected_account))
             },
             trailingIcon = {
                 ExposedDropdownMenuDefaults.TrailingIcon(
@@ -97,6 +115,8 @@ private fun DropDownView(accounts: List<Account>) {
             }
         )
         ExposedDropdownMenu(
+            modifier = Modifier
+                .fillMaxWidth(),
             expanded = expanded,
             onDismissRequest = {
                 expanded = false
@@ -117,10 +137,73 @@ private fun DropDownView(accounts: List<Account>) {
     }
 }
 
+@Composable
+@OptIn(ExperimentalMaterial3Api::class)
+private fun CategoryPreSelectionView(
+    categories: List<Category>,
+    modifier: Modifier = Modifier
+) {
+    var expanded by remember { mutableStateOf(false) }
+    var selectedAccount by remember { mutableStateOf(categories[0]) }
+
+    ExposedDropdownMenuBox(
+        modifier = modifier,
+        expanded = expanded,
+        onExpandedChange = {
+            expanded = expanded.not()
+        }
+    ) {
+        OutlinedTextField(
+            modifier = Modifier
+                .fillMaxWidth()
+                .menuAnchor(),
+            readOnly = true,
+            value = selectedAccount.name,
+            onValueChange = { },
+            label = {
+                Text(stringResource(id = R.string.default_selected_category))
+            },
+            trailingIcon = {
+                ExposedDropdownMenuDefaults.TrailingIcon(
+                    expanded = expanded
+                )
+            }
+        )
+        ExposedDropdownMenu(
+            modifier = Modifier
+                .fillMaxWidth(),
+            expanded = expanded,
+            onDismissRequest = {
+                expanded = false
+            }
+        ) {
+            categories.forEach { account ->
+                DropdownMenuItem(
+                    text = {
+                        Text(text = account.name)
+                    },
+                    onClick = {
+                        selectedAccount = account
+                        expanded = false
+                    }
+                )
+            }
+        }
+    }
+}
+
 fun getRandomAccountData(totalCount: Int = 10): List<Account> {
     return buildList {
         repeat(totalCount) {
             add(getAccountData(it))
+        }
+    }
+}
+
+fun getRandomCategoryData(totalCount: Int = 10): List<Category> {
+    return buildList {
+        repeat(totalCount) {
+            add(getCategoryData(it))
         }
     }
 }
@@ -144,12 +227,30 @@ fun getAccountData(
     )
 }
 
+fun getCategoryData(
+    index: Int,
+    categoryType: CategoryType = CategoryType.EXPENSE,
+): Category {
+    return Category(
+        id = "$index",
+        name = "Account $index",
+        type = categoryType,
+        storedIcon = StoredIcon(
+            name = "credit_card",
+            backgroundColor = "#000000",
+        ),
+        createdOn = Date(),
+        updatedOn = Date(),
+    )
+}
+
 @Preview
 @Composable
 fun AdvancedSettingsPreview() {
     ExpenseManagerTheme {
         AdvancedSettingsScaffoldView(
-            accounts = getRandomAccountData(5)
+            accounts = getRandomAccountData(5),
+            categories = getRandomCategoryData(5),
         ) {
 
         }
--- feature/settings/src/main/kotlin/com/naveenapps/expensemanager/feature/settings/AdvancedSettingsViewModel.kt
+++ feature/settings/src/main/kotlin/com/naveenapps/expensemanager/feature/settings/AdvancedSettingsViewModel.kt
@@ -6,7 +6,9 @@ import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.naveenapps.expensemanager.core.domain.usecase.account.GetAllAccountsUseCase
+import com.naveenapps.expensemanager.core.domain.usecase.category.GetAllCategoryUseCase
 import com.naveenapps.expensemanager.core.model.Account
+import com.naveenapps.expensemanager.core.model.Category
 import com.naveenapps.expensemanager.core.navigation.AppComposeNavigator
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.launchIn
@@ -15,6 +17,7 @@ import javax.inject.Inject
 
 @HiltViewModel
 class AdvancedSettingsViewModel @Inject constructor(
+    getAllCategoryUseCase: GetAllCategoryUseCase,
     getAllAccountsUseCase: GetAllAccountsUseCase,
     private val appComposeNavigator: AppComposeNavigator,
 ) : ViewModel() {
@@ -22,9 +25,16 @@ class AdvancedSettingsViewModel @Inject constructor(
     var accounts by mutableStateOf<List<Account>>(emptyList())
         private set
 
+    var categories by mutableStateOf<List<Category>>(emptyList())
+        private set
+
     init {
         getAllAccountsUseCase.invoke().onEach {
             accounts = it
+        }.launchIn(viewModelScope)
+
+        getAllCategoryUseCase.invoke().onEach {
+            categories = it
         }.launchIn(viewModelScope)
     }
 
