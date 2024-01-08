5252a79a5922d22212c45bc66c1569973ae635d6
Naveen Kumar Kuppan <naveenkumarn2@gmail.com>
2024-01-08 01:42:21 +0100

    Updating VERSION_NAME & VERSION_CODE

--- build-logic/convention/src/main/java/com/naveenapps/expensemanager/buildsrc/extensions/AndroidConfigExt.kt
+++ build-logic/convention/src/main/java/com/naveenapps/expensemanager/buildsrc/extensions/AndroidConfigExt.kt
@@ -11,8 +11,8 @@ const val TARGET_SDK = 34
 const val COMPILE_SDK: Int = 34
 const val MIN_SDK = 21
 
-const val VERSION_CODE = 13
-const val VERSION_NAME = "1.0.13"
+const val VERSION_CODE = 14
+const val VERSION_NAME = "1.0.14"
 
 const val BENCHMARK_RUNNER = "androidx.test.runner.AndroidJUnitRunner"
 
