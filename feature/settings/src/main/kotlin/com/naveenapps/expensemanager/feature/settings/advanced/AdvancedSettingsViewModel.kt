bd7856e43af51b96b3c96d265df2e61343883daf
Naveen Kumar Kuppan <naveenkumarn2@gmail.com>
2025-05-18 23:44:38 +0530

    Removed callbacks and streamlined them with actions to respond from the advanced settings screen

--- feature/settings/src/main/kotlin/com/naveenapps/expensemanager/feature/settings/advanced/AdvancedSettingsViewModel.kt
+++ feature/settings/src/main/kotlin/com/naveenapps/expensemanager/feature/settings/advanced/AdvancedSettingsViewModel.kt
@@ -6,6 +6,7 @@ import com.naveenapps.expensemanager.core.domain.usecase.account.GetAllAccountsU
 import com.naveenapps.expensemanager.core.domain.usecase.category.GetAllCategoryUseCase
 import com.naveenapps.expensemanager.core.model.Account
 import com.naveenapps.expensemanager.core.model.Category
+import com.naveenapps.expensemanager.core.model.Resource
 import com.naveenapps.expensemanager.core.model.isExpense
 import com.naveenapps.expensemanager.core.navigation.AppComposeNavigator
 import com.naveenapps.expensemanager.core.navigation.ExpenseManagerScreens
@@ -18,6 +19,7 @@ import kotlinx.coroutines.flow.firstOrNull
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -45,71 +47,73 @@ class AdvancedSettingsViewModel @Inject constructor(
     )
     val state = _state.asStateFlow()
 
-    private val _accounts = MutableStateFlow<List<Account>>(emptyList())
-    val accounts = _accounts.asStateFlow()
-
-    private val _selectedAccount = MutableStateFlow<Account?>(null)
-    val selectedAccount = _selectedAccount.asStateFlow()
-
-    private val _expenseCategories = MutableStateFlow<List<Category>>(emptyList())
-    val expenseCategories = _expenseCategories.asStateFlow()
-
-    private val _selectedExpenseCategory = MutableStateFlow<Category?>(null)
-    val selectedExpenseCategory = _selectedExpenseCategory.asStateFlow()
-
-    private val _incomeCategories = MutableStateFlow<List<Category>>(emptyList())
-    val incomeCategories = _incomeCategories.asStateFlow()
-
-    private val _selectedIncomeCategory = MutableStateFlow<Category?>(null)
-    val selectedIncomeCategory = _selectedIncomeCategory.asStateFlow()
-
     init {
         getAllAccountsUseCase.invoke().onEach { accounts ->
-            _accounts.value = accounts
-
             val accountId = settingsRepository.getDefaultAccount().firstOrNull()
             val account = accounts.find { it.id == accountId }
-            _selectedAccount.value = account ?: accounts.firstOrNull()
+            _state.update {
+                it.copy(
+                    accounts = accounts,
+                    selectedAccount = account ?: accounts.firstOrNull()
+                )
+            }
         }.launchIn(viewModelScope)
 
         getAllCategoryUseCase.invoke().onEach { categories ->
             val (expenses, incomes) = categories.partition { category -> category.type.isExpense() }
-            _expenseCategories.value = expenses
-            _incomeCategories.value = incomes
 
             val expenseCategoryId = settingsRepository.getDefaultExpenseCategory().firstOrNull()
             val expenseCategory = expenses.find { it.id == expenseCategoryId }
-            _selectedExpenseCategory.value = expenseCategory ?: expenses.firstOrNull()
 
             val incomeCategoryId = settingsRepository.getDefaultIncomeCategory().firstOrNull()
             val incomeCategory = incomes.find { it.id == incomeCategoryId }
-            _selectedIncomeCategory.value = incomeCategory ?: incomes.firstOrNull()
+
+            _state.update {
+                it.copy(
+                    expenseCategories = expenses,
+                    selectedExpenseCategory = expenseCategory ?: expenses.firstOrNull(),
+                    incomeCategories = expenses,
+                    selectedIncomeCategory = incomeCategory ?: incomes.firstOrNull(),
+                )
+            }
         }.launchIn(viewModelScope)
     }
 
     private fun changeDefaultAccount(account: Account) {
         viewModelScope.launch {
-            _selectedAccount.value = account
-            account.let {
-                settingsRepository.setDefaultAccount(it.id)
+            when (val response = settingsRepository.setDefaultAccount(account.id)) {
+                is Resource.Error -> Unit
+                is Resource.Success -> {
+                    if (response.data) {
+                        _state.update { it.copy(selectedAccount = account) }
+                    }
+                }
             }
         }
     }
 
     private fun changeSelectedExpenseCategory(category: Category) {
         viewModelScope.launch {
-            _selectedExpenseCategory.value = category
-            category.let {
-                settingsRepository.setDefaultExpenseCategory(it.id)
+            when (val response = settingsRepository.setDefaultExpenseCategory(category.id)) {
+                is Resource.Error -> Unit
+                is Resource.Success -> {
+                    if (response.data) {
+                        _state.update { it.copy(selectedExpenseCategory = category) }
+                    }
+                }
             }
         }
     }
 
     private fun changeSelectedIncomeCategory(category: Category) {
         viewModelScope.launch {
-            _selectedIncomeCategory.value = category
-            category.let {
-                settingsRepository.setDefaultIncomeCategory(it.id)
+            when (val response = settingsRepository.setDefaultIncomeCategory(category.id)) {
+                is Resource.Error -> Unit
+                is Resource.Success -> {
+                    if (response.data) {
+                        _state.update { it.copy(selectedIncomeCategory = category) }
+                    }
+                }
             }
         }
     }
@@ -118,44 +122,49 @@ class AdvancedSettingsViewModel @Inject constructor(
         appComposeNavigator.popBackStack()
     }
 
-    fun onItemSelection(item: Any) {
-        when (item) {
-            is Account -> {
-                changeDefaultAccount(item)
-            }
-
-            is Category -> {
-                if (item.type.isExpense()) {
-                    changeSelectedExpenseCategory(item)
-                } else {
-                    changeSelectedIncomeCategory(item)
-                }
-            }
-        }
-    }
-
     private fun openAccountsReOrder() {
         appComposeNavigator.navigate(ExpenseManagerScreens.AccountReOrderScreen)
     }
 
     fun processAction(action: AdvancedSettingAction) {
-        when (action) {
-            AdvancedSettingAction.Backup -> TODO()
-            AdvancedSettingAction.ClosePage -> {
-                closePage()
+        viewModelScope.launch {
+            when (action) {
+                AdvancedSettingAction.Backup -> {
+                    _event.send(AdvancedSettingEvent.Backup)
+                }
+
+                AdvancedSettingAction.ClosePage -> {
+                    closePage()
+                }
+
+                AdvancedSettingAction.OpenAccountReOrder -> {
+                    openAccountsReOrder()
+                }
+
+                AdvancedSettingAction.Restore -> {
+                    _event.send(AdvancedSettingEvent.Restore)
+                }
+
+                is AdvancedSettingAction.SelectAccount -> {
+                    changeDefaultAccount(action.account)
+                }
+
+                is AdvancedSettingAction.SelectExpenseCategory -> {
+                    changeSelectedExpenseCategory(action.category)
+                }
+
+                is AdvancedSettingAction.SelectIncomeCategory -> {
+                    changeSelectedIncomeCategory(action.category)
+                }
+
+                AdvancedSettingAction.DismissDateFilterDialog -> {
+                    _state.update { it.copy(showDateFilter = false) }
+                }
+
+                AdvancedSettingAction.ShowDateFilterDialog -> {
+                    _state.update { it.copy(showDateFilter = true) }
+                }
             }
-
-            AdvancedSettingAction.OpenAccountReOrder -> {
-                openAccountsReOrder()
-            }
-
-            AdvancedSettingAction.Restore -> {
-
-            }
-
-            is AdvancedSettingAction.SelectAccount -> TODO()
-            is AdvancedSettingAction.SelectExpenseCategory -> TODO()
-            is AdvancedSettingAction.SelectIncomeCategory -> TODO()
         }
     }
 }
